e5098b44b8a252966015d25ed21708de3f881778
Mert Temiz <merttemizjj@gmail.com>
2021-05-28 19:25:37 +0300

    model migration

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("<h2>Hello, world. You're at the polls index.</h2> <div><input type=checkbox id=vehicle1 name=vehicle1 value=bike><label for=vehicle1> I have a bike</label></div>")
