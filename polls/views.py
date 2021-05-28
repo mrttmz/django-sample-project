8c4792f0131921392e64bb861d2cc7db5a299020
Mert Temiz <merttemizjj@gmail.com>
2021-05-29 02:56:12 +0300

    added views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Hello, world. You're at the polls index.</h2> <div><input type=checkbox id=vehicle1 name=vehicle1 value=bike><label for=vehicle1> I have a bike</label></div>")
+    return HttpResponse("<h2>Hello, world. You're at the polls index.</h2>")
+
+def detail(request, question_id):
+
+    return HttpResponse("You are looking at question %s." %question_id)
+
+def results(request, question_id):
+
+    response = "You're looking at the result  of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+
+    return HttpResponse("You are voting on question %s" %question_id)
+
+
+    
+
+    
+
